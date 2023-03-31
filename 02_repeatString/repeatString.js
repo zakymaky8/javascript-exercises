02af42393d42f83717db8f35f72c9522e48971ca
zakymaky8 <zekaryasmekuaninit@gmail.com>
2023-03-31 09:18:29 -0400

    add a code block

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,7 @@
-const repeatString = function() {
-
+const repeatString = function(str,num) {
+  return str.repeat(num);
 };
+repeatString();
 
 // Do not edit below this line
 module.exports = repeatString;
